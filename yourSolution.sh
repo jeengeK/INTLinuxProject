36d40d15764434aa756935b636469195f8e9f433
jeengeK <ibensabo@gmail.com>
2024-06-23 21:19:07 +0300

    Merge remote-tracking branch 'origin/main'

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,9 +1,8 @@
+~/src$ cat yourSolution.sh
 #!/bin/bash
 echo "Generating secret..."
 secret=(814c5723c21e7e90a3eae36c8df3c513)
-# shellcheck disable=SC2128
 echo "Secret generated: $secret"
-# shellcheck disable=SC2086
-# shellcheck disable=SC2128
 echo $secret > .secret
+
 
